d7a0f716dc4862a42a0ec1fc7880940e7c55b6e4
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-09-11 15:20:41 +0500

    feat(db): update auth schema

--- src/db/authSchema.ts
+++ src/db/authSchema.ts
@@ -1,4 +1,4 @@
-import { pgTable, uuid, text, timestamp, integer, primaryKey } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 
 // 1. Users Table (Core Registration and Auth Data)
@@ -10,3 +10,24 @@ export const users = pgTable("user", {
   emailVerified: timestamp("emailVerified", { mode: "date" }),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
+
+/* Verification email schema */
+// export const emailVerificationTokens = pgTable(
+//   "email_verification_token",
+//   {
+//     id: uuid("id").defaultRandom().primaryKey(),
+
+//     userId: uuid("user_id")
+//       .notNull()
+//       .references(() => users.id, {
+//         onDelete: "cascade",
+//       }),
+
+//     token: varchar("token", { length: 255 }).notNull().unique(),
+
+//     expiresAt: timestamp("expires_at").notNull(),
+
+//     createdAt: timestamp("created_at").defaultNow().notNull(),
+//   }
+// );
+
