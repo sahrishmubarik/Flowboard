4f375c2d5a7c42b519442670356a0ce5d18cd433
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-09-24 12:55:59 +0500

    feat: Add board schema

--- src/db/boardSchema.ts
+++ src/db/boardSchema.ts
@@ -1,6 +1,6 @@
 import { users } from "./authSchema";
 import { workspace } from "./workspaceSchema";
-import { pgTable,uuid, varchar,timestamp} from "drizzle-orm/pg-core";
+import { pgTable,uuid, varchar,timestamp,  pgEnum,} from "drizzle-orm/pg-core";
 export const board=pgTable("board",
 {
     id:uuid("id").defaultRandom().primaryKey(),
@@ -21,3 +21,40 @@ export const board=pgTable("board",
 }
 
 );
+
+/* board Member table */
+export const boardMemberRoleEnum = pgEnum(
+  "board_member_role",
+  [
+    "owner",
+    "admin",
+    "manager",
+    "member",
+  ],
+);
+
+export const boardMembers = pgTable("board_members", {
+  id: uuid("id")
+    .defaultRandom()
+    .primaryKey(),
+
+  boardId: uuid("board_id")
+    .notNull()
+    .references(() => board.id, {
+      onDelete: "cascade",
+    }),
+
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id, {
+      onDelete: "cascade",
+    }),
+
+  role: boardMemberRoleEnum("role")
+    .notNull()
+    .default("member"),
+
+  createdAt: timestamp("created_at")
+    .defaultNow()
+    .notNull(),
+});
