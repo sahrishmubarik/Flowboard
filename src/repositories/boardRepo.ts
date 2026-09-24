70b80b620a20e3e85a40c86f5d46f31ba567eb33
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-09-24 12:58:30 +0500

    feat: add get board repo

--- src/repositories/boardRepo.ts
+++ src/repositories/boardRepo.ts
@@ -1,17 +1,70 @@
+import { db } from "@/db";
+import { board ,  boardMembers} from "@/db/boardSchema";
 
-export const BoardRepo={
-     async create( workspaceId: string, boardName:string, user_id:string) {
-        const [invitation] = await db
-          .select()
-          .from(invitationSchema)
-          .where(
-            and(
-              eq(invitationSchema.email, email),
-              eq(invitationSchema.workspaceId, workspaceId),
-              eq(invitationSchema.status, "PENDING"),
-            ),
-          );
-    
-        return invitation;
-      },
-}
+import { eq, and } from "drizzle-orm";
+export const BoardRepo = {
+  async create(
+    tx: typeof db,
+    workspaceId: string,
+    boardName: string,
+    createdBy: string,
+  ) {
+    // 1. Create board
+    const [boardDetails] = await tx
+      .insert(board)
+      .values({
+        organizationId: workspaceId,
+        boardName,
+        createdBy,
+      })
+      .returning({
+        id: board.id,
+        boardName: board.boardName,
+        organizationId: board.organizationId,
+        createdBy: board.createdBy,
+        createdAt: board.createdAt,
+      });
+
+    // 2. Add creator as board owner
+    await tx.insert(boardMembers).values({
+      boardId: boardDetails.id,
+      userId: createdBy,
+      role: "owner",
+    });
+
+    return boardDetails;
+  },
+
+  async getBoards(
+  workspaceId: string,
+  userId: string,
+) {
+  console.log("Workspace Id in getBoards:", workspaceId);
+  console.log("User Id in getBoards:", userId);
+const boards = await db
+  .select({
+    id: board.id,
+    boardName: board.boardName,
+    organizationId: board.organizationId,
+    createdBy: board.createdBy,
+    createdAt: board.createdAt,
+    role: boardMembers.role,
+  })
+  .from(board)
+  .innerJoin(
+    boardMembers,
+    eq(boardMembers.boardId, board.id),
+  )
+  .where(
+    and(
+      eq(board.organizationId, workspaceId),
+      eq(boardMembers.userId, userId),
+    ),
+  );
+
+console.log("BOARDS FROM DB:", boards);
+
+return { boards };
+
+},
+};
