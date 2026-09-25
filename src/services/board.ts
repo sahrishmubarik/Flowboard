4b7ad404aad8d92c6ede4ca2b51e52bdabbfe117
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-09-25 21:49:37 +0500

    update board service

--- src/services/board.ts
+++ src/services/board.ts
@@ -3,8 +3,9 @@ import { getCurrentUser } from "@/lib/middleware/auth";
 import { requireWorkspaceRole } from "@/lib/middleware/workspacePermission";
 import { validateData, boardValidation } from "@/lib/validations/board";
 import { BoardRepo } from "@/repositories/boardRepo";
-
+import { requireBoardRole } from "@/lib/middleware/boardPermission";
 import { db } from "@/db";
+
 /* create board */
 export async function createBoard(workspaceId: string, boardName: string) {
   const user = await getCurrentUser();
@@ -48,9 +49,9 @@ export async function getBoardByWorkspaceId(workspaceId: string) {
   }
   console.log(workspaceId);
   const user_id = user.userId;
-  const boardDetails = await BoardRepo.getBoards( workspaceId, user_id);
+  const boardDetails = await BoardRepo.getBoards(workspaceId, user_id);
   console.log("See here board response");
-console.log(boardDetails);
+  console.log(boardDetails);
   return Response.json(
     {
       message: "Fetch boards successfully",
@@ -58,6 +59,79 @@ console.log(boardDetails);
     },
     {
       status: 200,
+    },
+  );
+}
+
+/* GET BOARD THROUGH ITS ID */
+
+export async function getBoardById(boardId: string) {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    throw new AppError("Unauthorized user", 401);
+  }
+
+  const boardData = await BoardRepo.getForUser(user.userId, boardId);
+
+  if (!boardData) {
+    throw new AppError("Board not found", 404);
+  }
+
+  return Response.json(
+    {
+      message: "Fetched board  successfully",
+      board: boardData,
+    },
+    {
+      status: 200,
+    },
+  );
+}
+
+export async function updateBoardName(boardId: string, boardName: string) {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    throw new AppError("Unauthorized user", 401);
+  }
+  const user_id = user.userId;
+  const validation = validateData(boardValidation, { boardName });
+
+  if (!validation.success) {
+    throw new AppError(validation.error, 400);
+  }
+
+  const { boardName: validateBoardName } = validation.data;
+
+  await requireBoardRole(user_id, boardId, ["owner", "admin"]);
+  const result = BoardRepo.updateBoardName(boardId, validateBoardName);
+  return Response.json(
+    {
+      message: "Board Name update successfully",
+      workspace: result,
+    },
+    {
+      status: 201,
+    },
+  );
+}
+
+export async function deleteBoard(boardId: string) {
+  const user = await getCurrentUser();
+  if (!user) {
+    throw new AppError("Unauthorized user", 401);
+  }
+  const user_id = user.userId;
+  await requireBoardRole(user_id, boardId, ["owner"]);
+  const result = BoardRepo.deleteBoard(boardId);
+  return Response.json(
+    {
+      message: "Delete board successfully!",
+      board: result,
+    },
+    {
+      status: 201,
     },
   );
 }
