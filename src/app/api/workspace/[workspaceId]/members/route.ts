77d2f296b0a070b903e326593454bacfb4e0dfbc
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-09-25 21:53:35 +0500

    update workspace member endpoint

--- src/app/api/workspace/[workspaceId]/members/route.ts
+++ src/app/api/workspace/[workspaceId]/members/route.ts
@@ -3,6 +3,7 @@ import { AppError } from "@/lib/errors/AppError";
 import {
   getWorkspaceMembers,
   getWorkspaceMembersByRole,
+  deactivateUserFromOrganization,
 } from "@/services/workspaceMember";
 
 export async function GET(
@@ -13,26 +14,54 @@ export async function GET(
     const { workspaceId } = await params;
 
     const { searchParams } = new URL(request.url);
-    const page = Number(
-  searchParams.get("page") ?? "1",
-);
+    const page = Number(searchParams.get("page") ?? "1");
 
-const limit = Number(
-  searchParams.get("limit") ?? "5",
-);
+    const limit = Number(searchParams.get("limit") ?? "5");
 
-const role = searchParams.get("role");
-   
+    const role = searchParams.get("role");
 
     if (role) {
-      return await getWorkspaceMembersByRole(workspaceId, role, page ,limit);
+      return await getWorkspaceMembersByRole(workspaceId, role, page, limit);
     }
-  return await getWorkspaceMembers(workspaceId,
-        page,
-        limit,
-    );
+    return await getWorkspaceMembers(workspaceId, page, limit);
   } catch (error) {
     console.error("GET_WORKSPACE_MEMBERS_ERROR:", error);
+
+    if (error instanceof AppError) {
+      return NextResponse.json(
+        {
+          message: error.message,
+        },
+        {
+          status: error.statusCode,
+        },
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: "Internal server error. Please try again.",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
+type deActivatedUserBody = {
+  userId: string;
+};
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ workspaceId: string }> },
+) {
+  try {
+    const { workspaceId } = await params;
+    const body: deActivatedUserBody = await request.json();
+    const { userId } = body;
+    return await deactivateUserFromOrganization(userId, workspaceId);
+  } catch (error) {
+    console.error("UPDATE_WORKSPACE_MEMBERS_DEACTIVATE_ERROR:", error);
 
     if (error instanceof AppError) {
       return NextResponse.json(
