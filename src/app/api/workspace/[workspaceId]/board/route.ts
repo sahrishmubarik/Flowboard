063b93a6baf9a192d9b63eb3f7af90db73862da7
Sahrish3081 <sahrish1026aiml@gmail.com>
2026-09-25 21:47:52 +0500

    update workspace board endpoint

--- src/app/api/workspace/[workspaceId]/board/route.ts
+++ src/app/api/workspace/[workspaceId]/board/route.ts
@@ -1,65 +1,64 @@
 import { AppError } from "@/lib/errors/AppError";
 import { NextResponse } from "next/server";
 import { createBoard, getBoardByWorkspaceId } from "@/services/board";
-type BoardName={
-boardName:string;
+type BoardName = {
+  boardName: string;
 };
-export async function POST(request:Request,
-  {params}:{params: Promise<{workspaceId:string}>}
-){
- try{
-  const { workspaceId }=await params;
-  const body: BoardName=await request.json();
-  const {boardName}=body;
-    return await  createBoard(workspaceId, boardName);
- }
- catch (error) {
-     console.error("BOARD_API_ERROR:", error);
- 
-     if (error instanceof AppError) {
-       return NextResponse.json(
-         {
-           message: error.message,
-         },
-         { status: error.statusCode },
-       );
-     }
- 
-     return NextResponse.json(
-       {
-         message: "Internal server error. Please try again.",
-       },
-       { status: 500 },
-     );
-   }
+export async function POST(
+  request: Request,
+  { params }: { params: Promise<{ workspaceId: string }> },
+) {
+  try {
+    const { workspaceId } = await params;
+    const body: BoardName = await request.json();
+    const { boardName } = body;
+    return await createBoard(workspaceId, boardName);
+  } catch (error) {
+    console.error("BOARD_API_ERROR:", error);
+
+    if (error instanceof AppError) {
+      return NextResponse.json(
+        {
+          message: error.message,
+        },
+        { status: error.statusCode },
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: "Internal server error. Please try again.",
+      },
+      { status: 500 },
+    );
+  }
 }
 
+/* get boards in specific workspace */
 export async function GET(
   request: Request,
-{params}:{params: Promise<{workspaceId:string}>}
-){
-
-  try{
-     const { workspaceId }=await params;
+  { params }: { params: Promise<{ workspaceId: string }> },
+) {
+  try {
+    const { workspaceId } = await params;
     return await getBoardByWorkspaceId(workspaceId);
-  }
-  catch(error){
+  } catch (error) {
     console.error("GET_BOARD_DETAIL_API_ERROR:", error);
- 
-     if (error instanceof AppError) {
-       return NextResponse.json(
-         {
-           message: error.message,
-         },
-         { status: error.statusCode },
-       );
-     }
- 
-     return NextResponse.json(
-       {
-         message: "Internal server error. Please try again.",
-       },
-       { status: 500 },
-     );
+
+    if (error instanceof AppError) {
+      return NextResponse.json(
+        {
+          message: error.message,
+        },
+        { status: error.statusCode },
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: "Internal server error. Please try again.",
+      },
+      { status: 500 },
+    );
   }
 }
